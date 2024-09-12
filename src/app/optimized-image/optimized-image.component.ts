33f41a2108adc6e6ec228f3e48a16e9a001433da
Night-human <124959944+Night-human@users.noreply.github.com>
2024-09-12 22:29:28 +0000

    image optimization finished

--- src/app/optimized-image/optimized-image.component.ts
+++ src/app/optimized-image/optimized-image.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+// First, Add the import
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-optimized-image',
   standalone: true,
+  // Second, Import
   imports: [NgOptimizedImage],
   templateUrl: './optimized-image.component.html',
   styleUrl: './optimized-image.component.scss'
